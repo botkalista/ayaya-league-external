187fa17d0a32db8c5b22280bbd3308f02a751788
Emily <laura.emily.lovi@gmail.com>
2022-08-05 08:18:14 +0200

    dump moved to utils to add user scripts to scripts

--- src/utils/dump.ts
+++ src/utils/dump.ts
@@ -9,9 +9,9 @@ const leagueModule = AyayaMemoryReader.leagueModule;
 const baseAddress = leagueModule.modBaseAddr;
 const size = leagueModule.modBaseSize;
 
-if (fs.existsSync('dump/dump.hex')) fs.rmSync('dump/dump.hex');
-if (fs.existsSync('dump/dump.info')) fs.rmSync('dump/dump.info');
-const writeStream = fs.createWriteStream('dump/dump.hex', { flags: 'a', encoding: 'hex' });
+if (fs.existsSync('../dump/dump.hex')) fs.rmSync('../dump/dump.hex');
+if (fs.existsSync('../dump/dump.info')) fs.rmSync('../dump/dump.info');
+const writeStream = fs.createWriteStream('../dump/dump.hex', { flags: 'a', encoding: 'hex' });
 
 const CHUNK_SIZE = 10000;
 for (let i = 0; i < size * 20; i += CHUNK_SIZE) {
