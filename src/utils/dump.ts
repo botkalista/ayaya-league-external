11dd1a9aeb35e3248eb8c9a10f6bd4243bf10fe7
Emily <laura.emily.lovi@gmail.com>
2022-08-05 13:47:02 +0200

    fix path

--- src/utils/dump.ts
+++ src/utils/dump.ts
@@ -9,9 +9,9 @@ const leagueModule = AyayaMemoryReader.leagueModule;
 const baseAddress = leagueModule.modBaseAddr;
 const size = leagueModule.modBaseSize;
 
-if (fs.existsSync('../dump/dump.hex')) fs.rmSync('../dump/dump.hex');
-if (fs.existsSync('../dump/dump.info')) fs.rmSync('../dump/dump.info');
-const writeStream = fs.createWriteStream('../dump/dump.hex', { flags: 'a', encoding: 'hex' });
+if (fs.existsSync('./dump/dump.hex')) fs.rmSync('./dump/dump.hex');
+if (fs.existsSync('./dump/dump.info')) fs.rmSync('./dump/dump.info');
+const writeStream = fs.createWriteStream('./dump/dump.hex', { flags: 'a', encoding: 'hex' });
 
 const CHUNK_SIZE = 10000;
 for (let i = 0; i < size * 20; i += CHUNK_SIZE) {
@@ -19,4 +19,4 @@ for (let i = 0; i < size * 20; i += CHUNK_SIZE) {
     writeStream.write(dump);
 }
 
-fs.writeFileSync('../dump/dump.info', JSON.stringify({ baseAddress }));
+fs.writeFileSync('./dump/dump.info', JSON.stringify({ baseAddress }));
