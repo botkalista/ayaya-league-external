894736500e4d0e05a3db0ffc3c3b98d1de4af1d4
Emily <laura.emily.lovi@gmail.com>
2022-08-06 09:33:33 +0200

    moved safe delay on Wrapper

--- src/ActionControllerWrapper.ts
+++ src/ActionControllerWrapper.ts
@@ -4,16 +4,23 @@ import * as ws from 'ws';
 class ActionControllerWrapper {
     private socket: ws;
     private connected = false;
+    private lastActionTime = 0;
     connect() {
         if (this.connected) return;
         this.socket = new ws("ws://127.0.0.1:7007");
         this.connected = true;
     }
-    leftClick() { this.socket.send('leftClick') }
-    rightClick() { this.socket.send('rightClick') }
-    leftClickAt(x: number, y: number) { this.socket.send(`leftClickAt#${x}#${y}`) }
-    rightClickAt(x: number, y: number) { this.socket.send(`rightClickAt#${x}#${y}`) }
-    moveMouse(x: number, y: number) { this.socket.send(`moveMouse#${x}#${y}`) }
+    leftClick() { if (!this.canSend()) return; this.socket.send('leftClick'); }
+    rightClick() { if (!this.canSend()) return; this.socket.send('rightClick'); }
+    leftClickAt(x: number, y: number) { if (!this.canSend()) return; this.socket.send(`leftClickAt#${x}#${y}`); }
+    rightClickAt(x: number, y: number) { if (!this.canSend()) return; this.socket.send(`rightClickAt#${x}#${y}`); }
+    moveMouse(x: number, y: number) { if (!this.canSend()) return; this.socket.send(`moveMouse#${x}#${y}`); }
+    private canSend() {
+        const now = performance.now();
+        if (this.lastActionTime > now - 50) return false;
+        this.lastActionTime = now;
+        return true;
+    }
 }
 
 const instance = new ActionControllerWrapper();
--- threads/action_controller/ActionControllerThread.js
+++ threads/action_controller/ActionControllerThread.js
@@ -4,15 +4,10 @@ const server = new ws.Server({ port: 7007 });
 console.log('Listening on port 7007');
 
 
-
-let lastAction = 0;
-
 server.on('connection', socket => {
     console.log('Socket connected');
     socket.on('message', e => {
         try {
-            const now = performance.now();
-            if (lastAction > now - 50) return;
             const [action, ...args] = e.toString().split('#');
             ActionController[action](...args);
             lastAction = now;
