3d227a06fd9289b45227047d0b9f741b9cc78ba6
Emily <laura.emily.lovi@gmail.com>
2022-08-14 21:19:56 +0200

    fix onMoveCreate event

--- src/events/onMoveCreate.ts
+++ src/events/onMoveCreate.ts
@@ -3,20 +3,15 @@ import { Vector3 } from "../models/Vector";
 import { fnPublish, ScriptSettingsFull } from '../events/types';
 
 export function publishOnMoveCreate(manager: UserScriptManager, aiManagerCache: Map<string, [Vector3, Vector3]>, settings: ScriptSettingsFull, publish: fnPublish) {
-
     for (const champ of manager.champions.enemies) {
-
         const target = aiManagerCache.get(champ.name);
         if (target) {
             const oStart = target[0];
             const oEnd = target[1];
-            if (oStart && oEnd && oStart.isEqual(champ.AiManager.startPath) && oEnd.isEqual(champ.AiManager.endPath)) {
-                // console.log(champ.name);
+            if (!oStart.isEqual(champ.AiManager.startPath) || !oEnd.isEqual(champ.AiManager.endPath)) {
                 publish('onMoveCreate', settings, champ, manager);
             }
         }
         aiManagerCache.set(champ.name, [champ.AiManager.startPath.copy(), champ.AiManager.endPath.copy()]);
-
     }
-
 }
