88dd48b777255a67082c1fb8b97cf7397e964e1c
Emily <laura.emily.lovi@gmail.com>
2022-08-20 02:19:25 +0200

    add key setting

--- static/settings/settings.js
+++ static/settings/settings.js
@@ -1,6 +1,16 @@
 
 const { ipcRenderer } = require('electron');
 
+window.mouseEventFired = false;
+
+// console.log = (...args) => {
+//     document.getElementsByClassName('logs')[0].append(args.join(' ') + '\n');
+// }
+// console.error = (...args) => {
+//     document.getElementsByClassName('logs')[0].append(args.join(' ') + '\n');
+// }
+
+
 const state = Vue.reactive({
     base: {
         overlaySpells: false,
@@ -16,7 +26,7 @@ ipcRenderer.on('dataSettings', function (evt, message) {
         if (a.name == 'Core.js') return -1;
         if (b.name == 'Core.js') return 1;
         return 0;
-    })
+    });
 });
 ipcRenderer.send('requestSettings');
 
@@ -24,6 +34,25 @@ ipcRenderer.send('requestSettings');
 const app = Vue.createApp({
     data() { return state; },
     methods: {
+        parseKey(setting) {
+            return setting.strValue;
+        },
+        unselect(evt) {
+            evt.target.selectionEnd = 0;
+        },
+        setMouse(setting, evt) {
+            if (evt.which != 4) return;
+            setting.value = 0x5;
+            setting.strValue = 'MBx2';
+            mouseEventFired = true;
+            ipcRenderer.send('updateSettings', JSON.stringify(state.scripts));
+        },
+        setKey(setting, evt) {
+            if (mouseEventFired) return mouseEventFired = false;
+            setting.value = evt.keyCode;
+            setting.strValue = evt.code; //`${evt.code} (0x${evt.keyCode.toString(16).toUpperCase()})`;
+            ipcRenderer.send('updateSettings', JSON.stringify(state.scripts));
+        },
         updateBaseSettings() {
             ipcRenderer.send('updateBaseSettings', JSON.stringify(state.base));
         },
