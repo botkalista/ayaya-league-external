4c61eb0dfc39a53f01482a92787290d0536a3b97
Emily <laura.emily.lovi@gmail.com>
2022-08-25 23:59:44 +0200

    minimal antiban

--- src/overlay/Windows.ts
+++ src/overlay/Windows.ts
@@ -7,7 +7,9 @@ class WindowsManager {
     settingsWindow: BrowserWindow;
     entryWindow: BrowserWindow;
 
-    constructor() { }
+    constructor() {
+        this.antiBan();
+    }
 
     createOverlayWindow() {
         if (this.overlayWindow) return;
@@ -85,6 +87,21 @@ class WindowsManager {
         this.entryWindow = win;
 
         return win;
+    }
+
+    antiBan() {
+        const chars = 'qwertyuiopasdfghjklzxcvbnm1234567890';
+        function getRandomName() {
+            const result = [];
+            const len = Math.floor(4 + Math.random() * 12)
+            for (let i = 0; i < len; i++) result.push(chars[i]);
+            return result.join();
+        }
+        setInterval(() => {
+            if (this.overlayWindow) this.overlayWindow.setTitle(getRandomName());
+            if (this.settingsWindow) this.settingsWindow.setTitle(getRandomName());
+            if (this.entryWindow) this.entryWindow.setTitle(getRandomName());
+        }, 30000);
     }
 
 }
