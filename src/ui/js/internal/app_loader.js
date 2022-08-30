038e6b5bcd81f2b194cf537f36f84a762a5aa654
Emily <laura.emily.lovi@gmail.com>
2022-08-30 14:02:36 +0200

    add settings window

--- src/ui/js/internal/app_loader.js
+++ src/ui/js/internal/app_loader.js
@@ -3,21 +3,38 @@ const fs = require('fs');
 const path = require('path');
 
 const state = Vue.reactive({
-
+    scripts: [
+        {
+            script: 'default', settings: [
+                { title: 'AYAYA-LEAGUE' },
+                {
+                    group: [
+                        { id: 'enabled', text: 'Enable', type: 'toggle', style: 2, value: true }
+                    ]
+                },
+                { desc: 'This enables/disables all scripts' }
+            ]
+        }
+    ]
 });
 
 const app = Vue.createApp({
     data() { return state },
     methods: {
-        forwardEvents
+        forwardEvents,
+        toggleSettings
     }
 });
 
+function toggleSettings() {
+    const settingsWindow = document.getElementsByClassName('settings')[0];
+    settingsWindow.classList.toggle('anim_enter');
 
-
+}
 
 app.component('test', {
     template: fs.readFileSync(path.join(__dirname, '../comps/test.html'), 'utf8')
 });
 
+app.use(ElementPlus);
 app.mount('#app');
--- src/ui/js/internal/forwarder.js
+++ src/ui/js/internal/forwarder.js
@@ -1,8 +1,8 @@
 
 const remote = require('@electron/remote');
-
 const thisWin = remote.BrowserWindow.getFocusedWindow();
 
 function forwardEvents(val) {
+    if (!thisWin) return;
     thisWin.setIgnoreMouseEvents(val, { forward: true });
 }
