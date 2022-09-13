f8ccfccb145cb89f116c01ef8b63f22588df0a09
Emily <laura.emily.lovi@gmail.com>
2022-09-13 10:07:37 +0200

    update dev copy

--- dev/copy_files.js
+++ dev/copy_files.js
@@ -4,3 +4,4 @@ const fs = require('fs');
 
 fs.rmSync('./dist/components/winapi/cpp', { force: true, recursive: true })
 fs.cpSync('./src/components/winapi/cpp', './dist/components/winapi/cpp', { force: true, recursive: true });
+fs.cpSync('./src/offsets.min.js', './dist/offsets.min.js', { force: true, recursive: true });
